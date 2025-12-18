265515772a04c67c5cb723266497dd600c993f91
Gopinath21052006 <gopin6038@gmail.cam>
2025-12-18 23:43:12 +0530

    Add Vercel rewrites for pages\8

--- server.js
+++ server.js
@@ -51,6 +51,7 @@ app.use(express.static('public'));
 const userRoutes = require('./routes/userRoutes');
 const quizRoutes = require('./routes/quizRoutes');
 const adminRoutes = require('./routes/adminRoutes');
+
 /* =========================
    API ROUTES
 ========================= */
@@ -82,21 +83,21 @@ app.post('/api/test-post', (req, res) => {
 /* =========================
    PAGE ROUTES
 ========================= */
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/public/index.html');
-// });
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
-// app.get('/quiz', (req, res) => {
-//   res.sendFile(__dirname + '/public/quiz.html');
-// });
+app.get('/quiz', (req, res) => {
+  res.sendFile(__dirname + '/public/quiz.html');
+});
 
-// app.get('/leaderboard', (req, res) => {
-//   res.sendFile(__dirname + '/public/leaderboard.html');
-// });
+app.get('/leaderboard', (req, res) => {
+  res.sendFile(__dirname + '/public/leaderboard.html');
+});
 
-// app.get('/admin', (req, res) => {
-//   res.sendFile(__dirname + '/public/admin.html');
-// });
+app.get('/admin', (req, res) => {
+  res.sendFile(__dirname + '/public/admin.html');
+});
 
 /* =========================
    404 HANDLER (MUST BE LAST)
@@ -113,4 +114,10 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Something went wrong!' });
 });
 
-module.exports = app; 
+/* =========================
+   START SERVER
+========================= */
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`📊 Admin panel: http://localhost:${PORT}/admin`);
+});
